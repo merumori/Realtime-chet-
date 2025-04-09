bfc73de754184804f7497a9f39264c68fff5c803
Mori Meru <143690709+merumori@users.noreply.github.com>
2025-04-09 18:29:31 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,7 +10,7 @@ export default function Login({ setUser, setPage }) {
 
   const login = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { username, password });
+      const res = await axios.post('https://realtime-chet.onrender.com/api/auth/login', { username, password });
       setUser(res.data);
       setPage('chat');
     } catch (err) {
